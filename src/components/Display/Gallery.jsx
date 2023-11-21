8bfb0a169189f22dd9b78f52acad7cdc16ad2820
elangbagaskaraputra <elangbagaskara.putra@gmail.com>
2023-11-21 11:06:52 +0700

    hai

--- src/components/Display/Gallery.jsx
+++ src/components/Display/Gallery.jsx
@@ -2,22 +2,28 @@ import React, { useState } from 'react';
 
 function Gallery() {
     const [images, setImages] = useState([]);
+    const [filteredImages, setFilteredImages] = useState([]);
     const [selectedImage, setSelectedImage] = useState(null);
     const [rotation, setRotation] = useState(0);
     const [scaleX, setScaleX] = useState(1)
     const [scaleY, setScaleY] = useState(1)
+    const [newName, setNewName] = useState('');
+    const [position, setPosition] = useState(0);
 
     const handleImageUpload = (event) => {
         const file = event.target.files[0];
         const url = URL.createObjectURL(file);
-        setImages([...images, url]);
+        const newImages = [...images, {name: file.name, url: url}];
+        setImages(newImages);
+        setFilteredImages(newImages);
     };
 
     const handleImageSelect = (index) => {
-        setSelectedImage(images[index]);
+        setSelectedImage(filteredImages[index].url);
+        setNewName(filteredImages[index].name);
     };
 
-    const handleRotationChange = (event) => {
+        const handleRotationChange = (event) => {
         setRotation(event.target.value);
     };
 
@@ -29,6 +35,18 @@ function Gallery() {
         setScaleY(event.target.value);
     }
 
+    const handlePositionChange = (event) => {
+        setPosition(event.target.value);
+    }
+
+    const handleDeleteGallery = () => {
+        const newImages = images.filter(image => image.url !== selectedImage);
+        setImages(newImages);
+        setFilteredImages(newImages);
+        setSelectedImage(null);
+    }
+
+
     const handleDelete = () => {
         setImages(images.filter(image => image !== selectedImage));
         setSelectedImage(null);
@@ -36,18 +54,47 @@ function Gallery() {
 
     const handleImageSearch = (event) => {
         const search = event.target.value;
-        const filteredImages = images.filter(image => image.includes(search));
-        setImages(filteredImages);
+        if (search === '') {
+            setFilteredImages(images);
+        } else {
+            const filtered = images.filter(image => image.name.includes(search));
+            setFilteredImages(filtered);
+        }
     }
+
+    const handleReset = () => {
+        setRotation(0);
+        setScaleX(1);
+        setScaleY(1);
+    }
+
+    const handleRename = () => {
+        if (newName) {
+            const newImages = images.map(image => {
+                if (image.url === selectedImage) {
+                    return {name: newName, url: image.url};
+                } else {
+                    return image;
+                }
+            });
+            setImages(newImages);
+            setFilteredImages(newImages);
+            setNewName('');
+        }
+    }
+
 
     return (
         <div className="flex">
-            <input type="file" accept="image/*" onChange={handleImageUpload} /><br/><br/>
-            <input type={""}/>
+            <div className={"p-4 w-1/4"}>
+                <input type="file" accept="image/*" onChange={handleImageUpload} /><br/><br/>
+                <h1> Search: </h1>
+                <input type={"search"} onChange={handleImageSearch} className={"border-2"}/>
+            </div>
             <div className="w-3/4">
                 <div className="container object-contain aspect-[3840/896] bg-black overflow-hidden">
                     {selectedImage && (
-                        <div style={{ transform: `rotate(${rotation}deg) scaleX(${scaleX}) scaleY(${scaleY})` }} className="w-full h-full">
+                        <div style={{ transform: `rotate(${rotation}deg) scaleX(${scaleX}) scaleY(${scaleY}) ` }} className="w-full h-full">
                             <img
                                 src={selectedImage}
                                 alt=""
@@ -57,11 +104,11 @@ function Gallery() {
                     )}
                 </div>
                 <div className="flex flex-wrap">
-                    {images.map((src, index) => (
+                    {filteredImages.map((image, index) => (
                         <img
-                            key={src}
-                            src={src}
-                            alt=""
+                            key={image.url}
+                            src={image.url}
+                            alt={image.name}
                             className="w-64 h-64 object-contain cursor-pointer"
                             onClick={() => handleImageSelect(index)}
                         />
@@ -69,13 +116,24 @@ function Gallery() {
                 </div>
             </div>
             <div className="w-1/4 p-4">
-                <h1> Rotation: </h1> <input type={"number"} onChange={handleRotationChange} className={"border-2"}/>
+                <h1> Rename: </h1>
+                <input type={"text"} onChange={(event) => setNewName(event.target.value)} value={newName} className={"border-2"}/>
+                <button onClick={handleRename} className={"border-2 m-1"}>Rename</button>
+
+                <h1> Rotation: </h1> <input type={"number"} value={rotation} onChange={handleRotationChange} className={"border-2"}/>
                 <input type="range" min="-360" max="360" value={rotation} onChange={handleRotationChange} /> <br/> <br/>
-                <h1> Scale Vertical </h1> <input type={"number"} value={scaleX} step={"0.01"} onChange={handleScaleXChange} className={"border-2"}/>
+
+                <h1> Scale Horizontal </h1> <input type={"number"} value={scaleX} step={"0.01"} onChange={handleScaleXChange} className={"border-2"}/>
                 <input type={"range"} min={"0"} max={"10"} step={"0.01"}  value={scaleX} onChange={handleScaleXChange}/><br/> <br/>
-                <h1> Scale Horizontal </h1> <input type={"number"} onChange={handleScaleYChange} className={"border-2"}/>
+
+                <h1> Scale Vertical </h1> <input type={"number"} value={scaleY} onChange={handleScaleYChange} className={"border-2"}/>
                 <input type={"range"} min={"0"} max={"10"} step={"0.01"}  value={scaleY} onChange={handleScaleYChange}/> <br/> <br/>
-                <button onClick={handleDelete} className={"border-2"}>Delete</button>
+
+                <button onClick={handleReset} className={"border-2 m-1"}> Reset </button>
+                <button onClick={handleDelete} className={"border-2 m-1"}>Hide Layer</button>
+                <button onClick={handleDeleteGallery} className={"border-2 m-1"}>Delete</button>
+            </div>
+            <div className={"w-1/4 bg-blue-300"}>
             </div>
         </div>
     );
--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import DisplayerPage from "../components/Display/DisplayerPage.jsx";
+import Gallery from "../components/Display/Gallery.jsx";
+import App from "../App.jsx";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/DisplayerPage">
                 <DisplayerPage/>
+            </ComponentPreview>
+            <ComponentPreview path="/Gallery">
+                <Gallery/>
+            </ComponentPreview>
+            <ComponentPreview path="/App">
+                <App/>
             </ComponentPreview>
         </Previews>
     )
